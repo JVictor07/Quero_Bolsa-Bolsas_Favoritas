395fa0f21c3407fd46d3b99cde7c8b8e90a02f45
João Silva <58793114+JVictor07@users.noreply.github.com>
2020-03-26 22:34:11 -0300

    Script final

--- scripts/filtros.js
+++ scripts/filtros.js
@@ -5,10 +5,10 @@ fetch('db.json')
 	.then(function (ObjetosDoJson) {
 
 		FiltrarPorCurso(), FiltrarPorCidade(), FiltrarPorCheckBox(), FiltrarPorRange(), OrdenarPorNomeDaFaculdade()
-		
+
 		function FiltrarPorCurso() {
 			document.querySelector('select#selecione_curso').onchange = function (e) {
-				
+
 				nome_curso_selecionado = e.target.value
 
 				for (i = 0; i < ObjetosDoJson.length; i++) {
@@ -26,10 +26,10 @@ fetch('db.json')
 				}
 			}
 		}
-		
+
 		function FiltrarPorCidade() {
 			document.querySelector('select#selecione_cidade').onchange = function (e) {
-				
+
 				nome_cidade_selecionada = e.target.value
 
 				for (i = 0; i < ObjetosDoJson.length; i++) {
@@ -47,55 +47,55 @@ fetch('db.json')
 				}
 			}
 		}
-		
+
 		function FiltrarPorCheckBox() {
 
 			checkbox_distancia = document.getElementById('modalidade_distancia')
 			checkbox_presencial = document.getElementById('modalidade_presencial')
 
-			document.getElementById("tipo-curso").addEventListener("click", function (){
+			document.getElementById("tipo-curso").addEventListener("click", function () {
 
 				var modalidade_selecionada
 
 				for (i = 0; i < ObjetosDoJson.length; i++) {
 
 					if (checkbox_distancia.checked || checkbox_presencial.checked) {
-						
-						if(checkbox_distancia.checked && checkbox_presencial.checked) {
-							
+
+						if (checkbox_distancia.checked && checkbox_presencial.checked) {
+
 							document.querySelector('div.PosicaoJson' + i).classList.remove('nao-mostrar-facul3')
-						
-						} else if (checkbox_distancia.checked){
-	
+
+						} else if (checkbox_distancia.checked) {
+
 							modalidade_selecionada = "EaD"
 
-								if (modalidade_selecionada != ObjetosDoJson[i].course.kind) {
-									document.querySelector('div.PosicaoJson' + i).classList.add('nao-mostrar-facul3')
-								}
-								if (modalidade_selecionada == ObjetosDoJson[i].course.kind) {
-									document.querySelector('div.PosicaoJson' + i).classList.remove('nao-mostrar-facul3')
-								}
-	
-						} else if (checkbox_presencial.checked){
-	
+							if (modalidade_selecionada != ObjetosDoJson[i].course.kind) {
+								document.querySelector('div.PosicaoJson' + i).classList.add('nao-mostrar-facul3')
+							}
+							if (modalidade_selecionada == ObjetosDoJson[i].course.kind) {
+								document.querySelector('div.PosicaoJson' + i).classList.remove('nao-mostrar-facul3')
+							}
+
+						} else if (checkbox_presencial.checked) {
+
 							modalidade_selecionada = "Presencial"
 
-								if (modalidade_selecionada != ObjetosDoJson[i].course.kind){
-									document.querySelector('div.PosicaoJson' + i).classList.add('nao-mostrar-facul3')
-								}
-								if (modalidade_selecionada == ObjetosDoJson[i].course.kind) {
-									document.querySelector('div.PosicaoJson' + i).classList.remove('nao-mostrar-facul3')
-								}
+							if (modalidade_selecionada != ObjetosDoJson[i].course.kind) {
+								document.querySelector('div.PosicaoJson' + i).classList.add('nao-mostrar-facul3')
+							}
+							if (modalidade_selecionada == ObjetosDoJson[i].course.kind) {
+								document.querySelector('div.PosicaoJson' + i).classList.remove('nao-mostrar-facul3')
+							}
 						}
 
-					}else {
+					} else {
 						document.querySelector('div.PosicaoJson' + i).classList.remove('nao-mostrar-facul3')
 					}
-					
+
 				}
 			})
 		}
-		
+
 		function FiltrarPorRange() {
 			// Sempre que o valor da faculdade for maior que o valor selecionado pelo range, ela será removida.
 			input_range = document.getElementById('range-pd-pagar')
@@ -111,12 +111,12 @@ fetch('db.json')
 				}
 			}
 		}
-		
+
 		function OrdenarPorNomeDaFaculdade() {
 
-			CriarSpan(),OrganizarListaOrdemAlfabetica(), CriarElementosComNomesOrganizados()
-			
-			function CriarSpan(){
+			CriarSpan(), OrganizarListaOrdemAlfabetica(), CriarElementosComNomesOrganizados()
+
+			function CriarSpan() {
 				//Cria um span que ajudará na lógica de organização por ordem alfabética
 				secao_lifaculdades = document.querySelector('section#resultados')
 				primeiradiv = document.firstElementChild.querySelector('div.faculdades')
@@ -124,16 +124,16 @@ fetch('db.json')
 
 				secao_lifaculdades.insertBefore(span_criada, primeiradiv)
 			}
-			
-			function OrganizarListaOrdemAlfabetica(){
+
+			function OrganizarListaOrdemAlfabetica() {
 				//Se a opção "Ordenar por nome da faculdade" estiver selecionado,
 				//Cria uma lista com os nomes das faculdades ordenados por ordem alfabética
 				//Essa lista servirá para comparação na função abaixo.
 
-				Opcao_ordenar_por_facul = document.querySelector('select#Ordenar_por option') 
+				Opcao_ordenar_por_facul = document.querySelector('select#Ordenar_por option')
 
 				if (Opcao_ordenar_por_facul.selected) {
-					
+
 					arr_faculs = new Array
 					li_faculdades = document.querySelectorAll('section#resultados div.faculdades')
 
@@ -144,28 +144,27 @@ fetch('db.json')
 					}
 				}
 			}
-			
-			function CriarElementosComNomesOrganizados(){
+
+			function CriarElementosComNomesOrganizados() {
 				// Rotina que pesquisa o nome das universidades de cada div, as comparam com o array organizado e,
 				// a partir dessa comparação, as organizam para que fiquem em uma posição de nomes igual a do array 
-				
+
 				y = 0
 
 				while (y < ObjetosDoJson.length) {
 					for (i = 0; i < ObjetosDoJson.length; i++) {
 
-						if(ObjetosDoJson[i].university.name == arr_faculs[y] ){
+						if (ObjetosDoJson[i].university.name == arr_faculs[y]) {
 							div_dafacul = document.querySelector(`div.PosicaoJson${[i]}`)
 							span_criado = document.querySelector('section#resultados > span')
 							document.querySelector('section#resultados').insertBefore(div_dafacul, span_criado)
-							
-							y = y+1
-							}
+
+							y = y + 1
 						}
 					}
-			
+				}
+
 			}
 
 		}//Fim OrdenarPorNomeDaFaculdade
 	})
-	
